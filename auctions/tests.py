8554713c523aad78c873387fca23d19712787826
Leenus <LeenusGit@users.noreply.github.com>
2018-10-12 17:15:36 +0300

    added tests and removed unnecesary templates

--- auctions/tests.py
+++ auctions/tests.py
@@ -1,7 +1,9 @@
 import datetime
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import Auction
@@ -21,16 +23,31 @@ def createDefinedAuction():
 
 def createDefinedUser():
 
-    userName = 'JLennon'
+    username = 'JLennon'
     firstname = 'John'
     lastName = 'Lennon'
     email = 'lennon@thebeatles.com'
     password = 'johnpassword'
 
-    user = User.objects.create_user(username=userName, email=email, password=password)
+    user = User.objects.create_user(username=username, email=email, password=password)
     user.first_name = firstname
     user.last_name = lastName
     return user
+
+
+class AuctionIndexViewTests(TestCase):
+
+    def testNoAuctions(self):
+        response = self.client.get(reverse('auctions:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['auctionList'], [])
+       # self.assertContains(response, "No auctions are available.")
+
+    def testOneAuction(self):
+        auction = createDefinedAuction()
+        context = '<Auction: %s>' % auction.title
+        response = self.client.get(reverse('auctions:index'))
+        self.assertQuerysetEqual(response.context['auctionList'], [context])
 
 
 class AuctionModelTests(TestCase):
@@ -43,7 +60,14 @@ class AuctionModelTests(TestCase):
 class UserModelTests(TestCase):
 
     def testNewUserName(self):
-
         user = createDefinedUser()
         self.assertEqual(user.first_name, 'John')
         self.assertEqual(user.last_name, 'Lennon')
+
+    def testAuthenticateUser(self):
+        user = createDefinedUser()
+        username = 'JLennon'
+        password = 'johnpassword'
+        user2 = authenticate(username=username, password=password)
+
+        self.assertEquals(user, user2)
--- auctions/views.py
+++ auctions/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import generic
+from .models import Auction, Bid
 
 
-class IndexView(generic.ListView):
+class AuctionIndexView(generic.ListView):
     template_name = 'auctions/index.html'
     context_object_name = 'auctionList'
 
     def get_queryset(self):
-        return
+        return Auction.objects.all()
